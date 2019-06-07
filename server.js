aa7b0a0fae1ec19e5a85315701c3f87bd506a3a2
Web Star <webcodestar@gmail.com>
2019-06-08 00:12:43 +1000

    connect with mongoose from mlab db.

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose
+    .connect(
+        process.env.MONGO_URI,
+        { useNewUrlParser: true })
+    .then(() => console.log("DB connected"))
+    .catch(err => console.log(err));
 
 const todos = [
     { task: 'Wash Car', completed: false },
@@ -14,27 +23,27 @@ const typeDefs = gql`
         getTodos: [Todo]
     }
 
-    type Mutation {
-        addTodo(task: String, completed: Boolean): Todo
-    }
+    # type Mutation {
+    #     addTodo(task: String, completed: Boolean): Todo
+    # }
 `;
 
-const resolvers = {
-    Query: {
-        getTodos: () => todos
-    },
-    Mutation: {
-        addTodo: (_, { task, completed }) => {
-            const todo = { task, completed };
-            todos.push(todo);
-            return todo;
-        }
-    }
-}
+// const resolvers = {
+//     Query: {
+//         getTodos: () => todos
+//     },
+//     Mutation: {
+//         addTodo: (_, { task, completed }) => {
+//             const todo = { task, completed };
+//             todos.push(todo);
+//             return todo;
+//         }
+//     }
+// }
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+    typeDefs
+    // resolvers
 });
 
 server.listen().then(({ url }) => {
